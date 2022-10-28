142646ce6a097df84b4bfc8e99565be3829f32dc
Anish-ASU <anair47@asu.edu>
2022-10-27 21:46:05 -0700

    Updated code for problem statement

--- urinals.java
+++ urinals.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class urinals {
     public static void main(String[] args) {
+        
+        //urinals obj = new urinals();
         
     }
     Boolean goodString( String str ) {  // checks to see if valid string 
         int i = 0;
         int j = str.length();
-        int err = 0;
         String arr[] = str.split("");
         for(i=0;i<j;i++){
             if(arr[i].equals("1")||arr[i].equals("0")){
@@ -17,4 +20,58 @@ public class urinals {
         return true;
         
    }
+   public int countUrinals(String arr){
+
+        
+        int len = arr.length();
+        String ur[] = arr.split("");
+        
+        int j = len;
+        int i = 0;
+        int count = 0;
+        int v = 0;
+        while(i != j) {
+            if (ur[i].equals("1")){
+                if(j>i+1){
+                    if(ur[i+1].equals("1")){
+                    v= -1;
+                    return v;}
+                }
+            }
+            if(i == 0){
+                if (j == 1){
+                    if(ur[i].equals("1")){
+                        System.out.println("edge1");
+                        return v;
+                    }
+                    else{
+                        System.out.println("edge2");
+                        return v + 1;
+                    }
+                }
+                else if(ur[i].equals("0") && ur[i+1].equals("0")){
+                    ur[i] = "1";
+                    count++;
+                }
+            }
+            else if (i == j-1){
+                if(ur[i].equals("0") && ur[i-1].equals("0")){
+                    ur[i] = "1";
+                    count++;
+                
+                }
+            }
+            else {
+                if(ur[i].equals("0") && ur[i-1].equals("0") && ur[i+1].equals("0")){
+                    ur[i] = "1";
+                    count++;
+                }
+            }
+                
+            i++;
+
+        }
+        return count;
+
+    }
 }
